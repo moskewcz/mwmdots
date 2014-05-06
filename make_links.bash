7a2ee29936b4ba31a3e7e4aea04fd34ab5304b5d
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2014-05-06 14:44:28 -0700

    add gitconfig (note: uses @alum email)

--- make_links.bash
+++ make_links.bash
@@ -2,5 +2,6 @@ cd
 ln -s git_work/mwmdots/snippets 
 ln -s git_work/mwmdots/dot_emacs .emacs
 ln -s git_work/mwmdots/dot_bash_aliases .bash_aliases
+ln -s git_work/mwmdots/dot_gitconfig .gitconfig
 ln -s git_work/mwmdots/bin
 cd -
